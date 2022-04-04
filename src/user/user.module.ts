234b7b2811603c3a6541c647bd63957dd94be9a9
Almas2002 <73034060+Almas2002@users.noreply.github.com>
2022-04-04 14:41:14 +0600

    fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,7 +4,7 @@ import { UserController } from './user.controller';
 import { AuthModule } from '../auth/auth.module';
 import { UserService } from './user.service';
 import { FileModule } from '../file/file.module';
-
+require('dotenv').config()
 @Module({
   controllers:[UserController],
   imports:[ClientsModule.register([
@@ -12,7 +12,7 @@ import { FileModule } from '../file/file.module';
       name: 'USER_SERVICE',
       transport: Transport.REDIS,
       options: {
-        url: process.env.AUTH_SERVICE,
+        url: process.env.USER_SERVICE,
       }
     },
   ]),AuthModule,FileModule],
