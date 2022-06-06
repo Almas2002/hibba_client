d475c9fc7becda8ee76f8357ffa9bfcb17b3ec1c
Almas2002 <almas.zhumakhanov.2002a@gmail.com>
2022-06-06 17:13:55 +0600

    fix:

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -373,6 +373,7 @@ export class ProfileService {
 
     async createWorker(dto: CreateWorkerDto) {
         const date = new Date(dto.date)
+        console.log(date)
         const user = await this.authService.createWorker({password: dto.password, phone: dto.phone})
         const profile = await this.profileRepository.save({user,description:"",age:0,date, ...dto,})
         await this.placeRepository.save({profile, city: {id: dto.cityId}, ...dto})
