dcaa43f5ce0913da92e531e0a59c08033440e838
Almas2002 <almas.zhumakhanov.2002a@gmail.com>
2022-06-07 18:11:57 +0600

    fix:

--- src/complaint/complaint.module.ts
+++ src/complaint/complaint.module.ts
@@ -4,10 +4,11 @@ import { Complaint } from './complaint.entity';
 import { ComplaintService } from './complaint.service';
 import { ComplaintController } from './complaint.controller';
 import { ProfileModule } from '../profile/profile.module';
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
   controllers:[ComplaintController],
   providers:[ComplaintService],
-  imports:[TypeOrmModule.forFeature([Complaint]),ProfileModule]
+  imports:[TypeOrmModule.forFeature([Complaint]),ProfileModule,AuthModule]
 })
 export class ComplaintModule {}
