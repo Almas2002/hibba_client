7bd9ad322c332e2419865ec7e94c49997bd3f1e0
Almas2002 <almas.zhumakhanov.2002a@gmail.com>
2022-06-20 13:23:28 +0600

    fix:

--- src/hadis/hadis.controller.ts
+++ src/hadis/hadis.controller.ts
@@ -1,19 +1,25 @@
-import {Body, Controller, Delete, Get, Injectable, Param, Post} from "@nestjs/common";
+import {Body, Controller, Delete, Get, Injectable, Param, Post, UseGuards} from "@nestjs/common";
 import {HadisService} from "./hadis.service";
 import {CreateCategoryDto} from "./dto/create-category.dto";
 import {ApiOperation, ApiProperty, ApiTags} from "@nestjs/swagger";
 import {CreateHadisDto} from "./dto/create-hadis.dto";
+import {RoleGuards} from "../auth/guard/role.guard";
+import {Role} from "../user/decorators/role.decorator";
+import {RoleEnums} from "../enums/role.enums";
 @ApiTags("hadis")
 @Controller("hadis")
 export class HadisController {
     constructor(private hadisService: HadisService) {
     }
-
+    @Role(RoleEnums.SUPER_ADMIN,RoleEnums.WORKER)
+    @UseGuards(RoleGuards)
     @ApiOperation({summary: "create category hadis (admin)"})
     @Post("category")
     create(@Body()dto: CreateCategoryDto) {
         return this.hadisService.createCategory(dto)
     }
+    @Role(RoleEnums.SUPER_ADMIN,RoleEnums.WORKER)
+    @UseGuards(RoleGuards)
     @ApiOperation({summary: "create  hadis (admin)"})
     @Post("/:id")
     createHadis(@Param('id')id: number, @Body()hadis: CreateHadisDto) {
@@ -35,11 +41,15 @@ export class HadisController {
     getHadis(@Param('id')id:number){
         return this.hadisService.getFullHadis(id)
     }
+    @Role(RoleEnums.SUPER_ADMIN,RoleEnums.WORKER)
+    @UseGuards(RoleGuards)
     @ApiOperation({summary: "delete  hadis category with Id (param) (admin)"})
     @Delete('category/:id')
     deleteCategory(@Param('id')id:number){
         return this.hadisService.deleteHadisCategory(id)
     }
+    @Role(RoleEnums.SUPER_ADMIN,RoleEnums.WORKER)
+    @UseGuards(RoleGuards)
     @ApiOperation({summary: "delete  hadis with Id (param) (admin)"})
     @Delete(':id')
     deleteHadis(@Param('id')id:number){
--- src/hadis/hadis.module.ts
+++ src/hadis/hadis.module.ts
@@ -5,11 +5,12 @@ import {TypeOrmCoreModule} from "@nestjs/typeorm/dist/typeorm-core.module";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Hadis} from "./hadis.entity";
 import {HadisCategory} from "./hadis-category.entity";
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
     controllers:[HadisController],
     providers:[HadisService],
-    imports:[TypeOrmModule.forFeature([Hadis,HadisCategory])]
+    imports:[TypeOrmModule.forFeature([Hadis,HadisCategory]),AuthModule]
 })
 export class HadisModule{
 
