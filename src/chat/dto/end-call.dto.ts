13f60cd0657cb55fe324e399f71c331d835af7a7
Almas2002 <almas.zhumakhanov.2002a@gmail.com>
2022-08-19 13:04:38 +0600

    fix

--- src/chat/dto/end-call.dto.ts
+++ src/chat/dto/end-call.dto.ts
@@ -4,5 +4,5 @@ export class EndCallDto{
     @ApiProperty({example: "Алмас", description: "имя который звонит дрогому пользователю"})
     firstName:string
     @ApiProperty({example: "1", description: "id которому надо позвонить"})
-    userId:number;
+    userId:number
 }
